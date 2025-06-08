f3a20a5ab180a1d02a38575364f9a8445310fb67
sankaranarayanan-code <sivavijaya125@gmail.com>
2025-06-08 14:26:14 +0530

    Dependencies without Injection

--- src/main/java/guru/springframework/spring_6_di/controllers/MyController.java
+++ src/main/java/guru/springframework/spring_6_di/controllers/MyController.java
@@ -1,12 +1,24 @@
 package guru.springframework.spring_6_di.controllers;
 
+import guru.springframework.spring_6_di.services.GreetingService;
+import guru.springframework.spring_6_di.services.GreetingServiceImpl;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
 
+    /** Dependency on GreetingService, but controller has all the control
+     * how the GreetingService is created and how it is managed.
+     * Now the controller doesn't play nicely with Spring Framework **/
+
+    private final GreetingService greetingService;
+
+    public MyController() {
+        this.greetingService = new GreetingServiceImpl();
+    }
+
     public String sayHello() {
         System.out.println("In Controller Class");
-        return "Hello Everyone!";
+        return greetingService.sayHello();
     }
 }
